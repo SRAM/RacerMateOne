679a850224b5011241ba1cc570d34d6b10019b69
Committer N <anon_n@example.com>
2018-01-16 14:19:23 -0800

    New racermate.dll that fixes the hitch in VT spinscan bar 18. Bumped version to 4.1.0.5 No other changes.

--- RacerMateOne/Properties/AssemblyInfo.cs
+++ RacerMateOne/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.1.0.4")]
-[assembly: AssemblyFileVersion("4.1.0.4")]
+[assembly: AssemblyVersion("4.1.0.5")]
+[assembly: AssemblyFileVersion("4.1.0.5")]
